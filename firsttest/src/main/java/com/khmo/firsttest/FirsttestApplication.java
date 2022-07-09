50bf964ebc0b2ef9cbd399d64eb8c21917f857be
Mohsin Khan <mhsn001@gmail.com>
2022-07-09 21:55:44 +0530

    added project

---
+++ firsttest/src/main/java/com/khmo/firsttest/FirsttestApplication.java
@@ -0,0 +1,13 @@
+package com.khmo.firsttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirsttestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirsttestApplication.class, args);
+	}
+
+}
---
+++ firsttest/src/test/java/com/khmo/firsttest/FirsttestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khmo.firsttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirsttestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
